e12efcb9cac5bcb39217a76a383a39a769c205c4
arc6828 <chavalit.kow@gmail.com>
2018-12-12 11:10:35 +0700

    first

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: VRU-COMPUTER
+ *
+ * Created on July 4, 2018, 1:26 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    int a;
+    printf("Hello World\n");
+    printf("Enter number : ");
+    scanf("%d", &a);
+    
+    printf("Display");
+    printf("Display : %d" , a);
+
+    return (EXIT_SUCCESS);
+}
+
